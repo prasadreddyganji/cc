0750dd0b5a0d2adc0ba641585cbe803dbc70b87c
prasadreddyganji <52129394+prasadreddyganji@users.noreply.github.com>
2021-06-09 13:50:24 +0530

    Update TitleTest.java

--- src/test/java/com/web/devproj/functional/TitleTest.java
+++ src/test/java/com/web/devproj/functional/TitleTest.java
@@ -14,7 +14,7 @@ public class TitleTest {
 		FirefoxOptions opts = new FirefoxOptions();
 		opts.setHeadless(true);
 		WebDriver driver= new FirefoxDriver(opts);
-		String baseUrl = "http://localhost:5050/devproj/index.html";
+		String baseUrl = "http://172.20.10.12:5050/devproj/index.html";
 		driver.get(baseUrl);
 		assertEquals("CurrencyConverter", driver.getTitle());
 		driver.close();
